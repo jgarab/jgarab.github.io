8768147d325d24608a2ebab3ce1e63eec2548de4
Jozsef Garab <garabj@hotmail.com>
2022-10-30 21:00:54 +0100

    modify

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,6 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'jgarab', // Usually your GitHub org/user name.
   projectName: 'jgarab.github.io', // Usually your repo name.
-  deploymentBranch: 'main',
   trailingSlash: false,
   
 
